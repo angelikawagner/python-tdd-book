62b75c26f3c242900b0a1b210658e564ff0e0928
angelikawagner <angelika.wagner@hotmail.com>
2023-03-18 18:06:08 -0500

    removed code

--- functional_test.py
+++ functional_test.py
@@ -1,9 +1,5 @@
 from selenium import webdriver
-#from selenium.webdriver import FirefoxOptions
 
-#opts = FirefoxOptions()
-#opts.add_argument("--headless")
-#browser = webdriver.Firefox(options=opts)
 browser = webdriver.Firefox()
 browser.get('http://localhost:8000')
 
